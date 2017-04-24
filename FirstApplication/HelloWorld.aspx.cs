78b4835a664b039faa27f0bc9064a6391b425438
Kpratiksha <pratiksha.koirala50@gmail.com>
2017-04-24 15:58:25 +0100

    My first Commit

---
+++ FirstApplication/HelloWorld.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FirstApplication
+{
+    public partial class HelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Button1.Text = "clicked";
+           
+            Label1.Text = "Hello How are you?";
+        }
+    }
+}
